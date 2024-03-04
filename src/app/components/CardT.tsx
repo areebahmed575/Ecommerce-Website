1189d137e1bff5e9a870ed09377c5001a6010a87
areebahmed575 <areebahmed575@gmail.com>
2024-03-04 18:20:53 +0500

    Card and ProductsCarousel added

---
+++ src/app/components/CardT.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import React, { FC } from "react";
+import Link from "next/link";
+import { client } from "@/lib/sanityClient"; 
+import imageUrlBuilder from "@sanity/image-url";
+import { oneProductType } from "./utlis/productDataAndTypes";
+
+const builder = imageUrlBuilder(client);
+
+function urlFor(source: any) {
+    return builder.image(source);
+}
+
+const CardT: FC<{ singleProductData: oneProductType }> = ({
+    singleProductData,
+  }) => {
+    console.log("singleProductData",singleProductData)
+    return (
+      <div className=" space-y-3 select-none hover:scale-110 duration-300">
+        <div className="relative w-full">
+          <div className="absolute inset-0 z-10" />
+          <Image
+            width={1000}
+            height={1000}
+            src={urlFor(singleProductData.image)
+              .url()}
+            alt={singleProductData.image.alt}
+          />
+        </div>
+        <div className="space-y-1 text-gray-600 font-semibold text-lg select-none">
+          <Link href={`/catalog/${singleProductData.slug.current}`}>
+            <h6>{singleProductData.productName}</h6>
+            <p>${singleProductData.price}</p>
+          </Link>
+        </div>
+      </div>
+    );
+  };
+  
+  export default CardT;
