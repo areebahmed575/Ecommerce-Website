c955d0d9da53bb0af1ab84b0f1df2087a1e8001a
areebahmed575 <areebahmed575@gmail.com>
2024-03-09 21:19:58 +0500

    Redux ToolKit added

---
+++ src/redux/store.tsx
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch,TypedUseSelectorHook, useSelector } from 'react-redux'
+
+
+
+export const store = configureStore({
+  reducer: {},
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
