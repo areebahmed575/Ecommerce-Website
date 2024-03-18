dc735babc6dffc2728d14dea868a6c8211a6dbc9
areebahmed575 <areebahmed575@gmail.com>
2024-03-19 02:57:33 +0500

    Basepath updated

--- src/app/components/Basepath.ts
+++ src/app/components/Basepath.ts
@@ -1,5 +1,5 @@
 export const BASE_PATH_FORAPI = process.env.NODE_ENV === "development" ?
     "http://localhost:3000"
-    : "https://https:/projectecom-fawn.vercel.app/"
+    : "https:/projectecom-fawn.vercel.app/"
 
 export default BASE_PATH_FORAPI
